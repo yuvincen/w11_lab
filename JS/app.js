6e416d671cb2fd79c9c9711cafe7ba3db010698e
VIncent Yu <vyu27@my.bcit.ca>
2021-03-24 15:04:26 -0700

    Creating todolist array

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,9 @@
 // your JavaScript goes here, you can delete this comment
+const todolist = [{todo: "clone repo for starter code", status:"complete"}, 
+    {todo: "add CSS and Javascript to Index.html", status:"complete"},
+    {todo: "create a for loop", status:"started"}, 
+    {todo: "create a second for loop", status:"started"}
+];
+
+const completedtodos = [];
+
